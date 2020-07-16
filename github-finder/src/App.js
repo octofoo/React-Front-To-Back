7283faafb30b878cdb23ab38fce3a8cbe1392a97
octofoo <michael.loffelmann@protonmail.com>
2020-07-16 14:08:34 +0200

    create Navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from "./components/layout/Navbar";
 import './App.css';
 
-class App extends React {
+class App extends Component {
 
     render() {
         return (
             <div className="App">
-                <h1>Hello React</h1>
+                <Navbar />
             </div>
         );
     }

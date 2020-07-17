13f52be6172879de1604b2bfae30c5f551b8d2ae
octofoo <michael.loffelmann@protonmail.com>
2020-07-17 09:30:52 +0200

    create Users

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,25 +1,18 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-    // state
-    state = {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-    }
 
     // output
     render() {
         // state variables
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
 
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt={login} className="round-img" style={{ width: "60px" }} />
 
                 <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1" target="_blank" rel="noopener noreferrer">More</a>
                 </div>
             </div>
         )
